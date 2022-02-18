e3f9b78023e781eb1916f63f53cdaac7040cfb35
Shobair Abidi <shobair.abidi@gmail.com>
2022-02-18 10:12:31 -0500

    initial commit

---
+++ FirebaseDemoApp/FirebaseDemoApp/LoginView/viewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseDemoApp
+//
+//  Created by Rave Training on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
