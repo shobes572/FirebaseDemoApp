e2f72dab5b566955aed80f8418dd79b61133b434
iGFarr <isaacgfarr@gmail.com>
2022-02-18 11:24:13 -0500

    Added outlets/actions; embedded views in nav controller

---
+++ FirebaseDemoApp/FirebaseDemoApp/UserView/viewControllers/UserViewVC.swift
@@ -0,0 +1,18 @@
+//
+//  UserViewVC.swift
+//  FirebaseDemoApp
+//
+//  Created by Isaac Farr on 2/18/22.
+//
+
+import UIKit
+
+class UserViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+}
